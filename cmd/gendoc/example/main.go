33f043da1160b5256a1f193869bebc3438c70d34
Alexander F. Rødseth <rodseth@gmail.com>
2023-05-17 18:15:18 +0200

    Make the "long" example a bit shorter, ref #5

--- cmd/gendoc/example/main.go
+++ cmd/gendoc/example/main.go
@@ -1,114 +1,41 @@
 package main
 
-/* This program renders a supersampled Mandelbulb.
- * It takes about 12 seconds to run on my laptop.
- * CC0 licensed.
- */
+import "fmt"
 
-import (
-	"fmt"
-	"image"
-	"image/color"
-	"image/png"
-	"math"
-	"os"
-	"sync"
-)
-
-const (
-	width, height      = 1920, 1080
-	ssWidth, ssHeight  = 3840, 2160
-	aspectRatio        = float64(ssWidth) / float64(ssHeight)
-	fov                = math.Pi / 8
-	maxIter            = 1000
-	power              = 9
-	escapeRadius       = 1.6
-	supersamplingRatio = 2
-)
-
-var wg sync.WaitGroup
-
-type vec3 struct{ x, y, z float64 }
-
-func (v vec3) add(w vec3) vec3    { return vec3{v.x + w.x, v.y + w.y, v.z + w.z} }
-func (v vec3) mul(s float64) vec3 { return vec3{v.x * s, v.y * s, v.z * s} }
-func (v vec3) length() float64    { return math.Sqrt(v.x*v.x + v.y*v.y + v.z*v.z) }
-func (v vec3) normalize() vec3    { return v.mul(1 / v.length()) }
-
-func mandelbulb(p vec3) float64 {
-	z, r, theta, phi := p, 0.0, 0.0, 0.0
-	for i := 0; i < maxIter; i++ {
-		r = z.length()
-		if r > escapeRadius {
-			break
-		}
-		theta, phi = math.Acos(z.z/r)*power, math.Atan2(z.y, z.x)*power
-		r = math.Pow(r, power)
-		z.x, z.y, z.z = p.x+r*math.Sin(theta)*math.Cos(phi), p.y+r*math.Sin(theta)*math.Sin(phi), p.z+r*math.Cos(theta)
+// calculateSquares calculates the sum of the squares of the digits of the given number
+// and sends the result to the squareop channel.
+func calculateSquares(number int, squareop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit
+		number /= 10
 	}
-	return r
+	squareop <- sum
 }
 
-func renderPixel(x, y int) color.Color {
-	px, py := (2*float64(x)/float64(ssWidth)-1)*aspectRatio*math.Tan(fov/2), (1-2*float64(y)/float64(ssHeight))*math.Tan(fov/2)
-	direction := vec3{px, py, -1}.normalize()
-	t := 0.0
-	for i := 0; i < maxIter; i++ {
-		p := direction.mul(t).add(vec3{0, 0, 5})
-		d := mandelbulb(p) - 1
-		if d < 0.001 {
-			return color.Gray{uint8(255 - t*255/100)}
-		}
-		t += d
-		if t > 100 {
-			break
-		}
+// calculateCubes calculates the sum of the cubes of the digits of the given number
+// and sends the result to the cubeop channel.
+func calculateCubes(number int, cubeop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit * digit
+		number /= 10
 	}
-	return color.Black
+	cubeop <- sum
 }
 
 func main() {
-	ssImg := image.NewRGBA(image.Rect(0, 0, ssWidth, ssHeight))
-	for y := 0; y < ssHeight; y++ {
-		wg.Add(1)
-		go func(y int) {
-			defer wg.Done()
-			for x := 0; x < ssWidth; x++ {
-				ssImg.Set(x, y, renderPixel(x, y))
-			}
-		}(y)
-	}
-	wg.Wait()
+	number := 589
+	sqrch := make(chan int)
+	cubech := make(chan int)
 
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			r, g, b, a := 0, 0, 0, 0
-			for dy := 0; dy < supersamplingRatio; dy++ {
-				for dx := 0; dx < supersamplingRatio; dx++ {
-					c := ssImg.RGBAAt(x*supersamplingRatio+dx, y*supersamplingRatio+dy)
-					r += int(c.R)
-					g += int(c.G)
-					b += int(c.B)
-					a += int(c.A)
-				}
-			}
-			r /= supersamplingRatio * supersamplingRatio
-			g /= supersamplingRatio * supersamplingRatio
-			b /= supersamplingRatio * supersamplingRatio
-			a /= supersamplingRatio * supersamplingRatio
-			img.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
-		}
-	}
+	// Start two goroutines to calculate the sum of squares and cubes of the digits.
+	go calculateSquares(number, sqrch)
+	go calculateCubes(number, cubech)
 
-	file, err := os.Create("mandelbulb.png")
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
-	err = png.Encode(file, img)
-	if err != nil {
-		fmt.Println("Error encoding image:", err)
-	}
+	// Receive the results from the channels and add them.
+	squares, cubes := <-sqrch, <-cubech
+	fmt.Println("Final result", squares+cubes)
 }
